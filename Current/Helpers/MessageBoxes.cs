c157bfe1beafb78f5405e9b0c435d326bf734c32
klanderfri <registrering+github@bjornlarsson.se>
2020-03-12 14:56:43 +0100

    Adding helper for message boxes.

---
+++ Current/Helpers/MessageBoxes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Current.Helpers
+{
+    public static class MessageBoxes
+    {
+        public static void ShowProblemWhenAddingDataMessage()
+        {
+            MessageBox.Show("A problem occured when adding new data!", "A problem occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
